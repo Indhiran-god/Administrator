c63a807f9838ee69d74f1ef0029b13379a30b388
Indhiran-god <110456933+Indhiran-god@users.noreply.github.com>
2024-11-07 07:55:55 +0530

    Update Footer.js

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer className='bg-slate-200 py-4'>
       <div className='text-center font-bold text-lg'>
-        KANI STORES
+        SIVA STORES
       </div>
       <div className='container mx-auto p-4 flex flex-col items-center justify-center'>
         <p className='text-center font-bold text-lg'>
