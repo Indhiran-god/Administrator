03c570115540f468511ef37664b8d12f30975e4e
Indhiran-god <110456933+Indhiran-god@users.noreply.github.com>
2024-11-12 19:13:03 +0530

    Update index.js

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -11,6 +11,7 @@ import Bills from '../pages/Bills';
 import Allsub from "../pages/Allsub"
 const router = createBrowserRouter([
     {
+        
         path: "*", // This will match all paths
         element: <App />, // Main app component
         children: [
